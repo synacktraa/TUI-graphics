a89f25cbe598d63f6b04c761b1874052b840e2ae
Mikey <harshmetaexploit90@gmail.com>
2022-03-09 19:23:28 +0530

    added color dictionary...

--- weirdshit.py
+++ weirdshit.py
@@ -1,28 +1,46 @@
 import time
 from sys import stdout
-from colorama import Fore, Style
+import os
 
-def weird_stdout(data, color=Fore.LIGHTWHITE_EX):
+"""
+🥴 Ansi codes weren't working in linux terminal, after using os.system("") it works like a charm 🥴
+"""
+os.system("")
 
-    stdout.write(f"{color}{data}{Style.RESET_ALL}")
 
-def get_cavity(data_len):
+cdict = {
     
-    spaces = str().expandtabs(8)
+    "BLACK":'\u001b[30m',
+    "RED":'\u001b[31m',	
+    "WHITE":'\u001b[37m',
+    "YELLOW":'\u001b[33m',
+    "BLUE":'\u001b[34m',
+    "GREEN":'\u001b[32m',	
+    "MAGENTA":'\u001b[35m',	
+    "CYAN":'\u001b[36m',	
+    "LIGHTGREEN_EX":'\u001b[32;1m',
+    "LIGHTCYAN_EX":'\u001b[36;1m',
+    "LIGHTBLUE_EX":'\u001b[34;1m',
+    "LIGHTRED_EX":'\u001b[31;1m',
+    "LIGHTMAGENTA_EX":'\u001b[35;1m',
+    "LIGHTYELLOW_EX":'\u001b[33;1m',
+    "LIGHTWHITE_EX":'\u001b[37;1m',
+    "LIGHTBLACK_EX":'\u001b[30;1m'
+}
 
-    while data_len:
-        if(data_len<8):
-            spaces += '\t'
-            for _ in range(8-data_len):
-                spaces += '\b'
-            data_len = 0
-        else:
-            spaces += '\t'
-            data_len -= 8
-            
-    return spaces
+RESET = '\u001b[0m'
+	
+
+def weird_stdout(data, color=cdict['LIGHTWHITE_EX']):
+
+    stdout.write(f"{color}{data}{RESET}")
+
+def weird_print(data, color=cdict['LIGHTWHITE_EX']):
+
+        print(f"{color}{data}{RESET}")    
     
-def weirdShit(): 
+
+def weirdShit(data_string="[+] loading script", schedule=3, thrust=4): 
 
     def spinning_cursor():
         while True:
@@ -31,18 +49,33 @@ def weirdShit():
                   
     spinner = spinning_cursor()
 
-    data = "[+] your message"
+    data = data_string
     upperstr = data.upper()
+    data_len = len(data)
 
-    for _ in range(3):
+    indent = str().expandtabs(8)
+
+    while data_len:
+        if(data_len<8):
+            indent += '\t'
+            for _ in range(8-data_len):
+                indent += '\b'
+            data_len = 0
+        else:
+            indent += '\t'
+            data_len -= 8
+
+    for _ in range(schedule):
+        
         for x in range(len(data)):
+
             s = '\r' + data[0:x] + upperstr[x] + data[x+1:] + '\r'
-            weird_stdout(s, Fore.GREEN)
-            weird_stdout(f"{get_cavity(len(data))}...")
+            weird_stdout(s, cdict['LIGHTGREEN_EX'])
+            weird_stdout(f"{indent}...")
             weird_stdout(next(spinner)) 
             weird_stdout("\r")
             stdout.flush()
-            time.sleep(0.1/0.8)
+            time.sleep(0.5/thrust)
 
 if __name__ == '__main__':
     weirdShit()
