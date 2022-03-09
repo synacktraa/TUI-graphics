37efee9bc7e9ad9a17bcfa58540cc87c71e72349
Mikey <harshmetaexploit90@gmail.com>
2022-03-09 19:44:31 +0530

    terminal borgir

--- illustration.py
+++ illustration.py
@@ -5,25 +5,25 @@ from sys import argv
 
 os.system("")
 
-def data_generate():
+def data_generate(data):
 
     """
         This is just an example file for illustration and showcasing
     """
 
-    file_list = ["joemama", "steal_your_momma", "sinister", "heheboi", "whyareyougay", "spaghetti"]
+    file_list = ["joemama", "st", "sinister", "heheboi", "whyareyougay", "spaghetti"]
     ext_list = ["png", "jpg", "mp4", "mov"]
     
 
     for file in file_list:
         ext = random.choice(ext_list)
-        weird_print(f"[+] Attaching {file}.{ext}", cdict['LIGHTBLACK_EX'])
+        weird_print(f"[+] Attaching {file}.{ext}{' '*len(data)}", cdict['LIGHTBLACK_EX'])
         sleep(0.8)
 
 def main(data, sdl, thrust):
 
     thread2 = threading.Thread(target=weirdShit, args=(data, sdl, thrust))
-    thread1 = threading.Thread(target=data_generate)
+    thread1 = threading.Thread(target=data_generate, args=(data,))
 
     threads =[thread1, thread2]
 
