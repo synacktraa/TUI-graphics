d78b31772bf6b88669fe75919a6583a62fd2e1a0
SynAcktraa <harshmetaexploit90@gmail.com>
2022-03-12 23:06:50 +0530

    removed weird attachments

--- illustration.py
+++ illustration.py
@@ -1,29 +1,29 @@
-from weirdshit import weirdShit, weird_print, cdict, os
+from weirdshit import graphic_gen, print_cw, cdict, os
 import threading, random
 from time import sleep
 from sys import argv
 
 os.system("")
 
-def data_generate(data):
+def data_generator(data):
 
     """
         This is just an example file for illustration and showcasing
     """
 
-    file_list = ["joemama", "steal_your_momma", "sinister", "heheboi", "whyareyougay", "spaghetti"]
-    ext_list = ["png", "jpg", "mp4", "mov"]
+    file_list = ["mom_spaghetti", "sinister", "static", "venom", "deadpool"]
+    ext_list = ["js", "go", "c", "asm"]
     
 
     for file in file_list:
         ext = random.choice(ext_list)
-        weird_print(f"[+] Attaching {file}.{ext}{' '*len(data)}", cdict['LIGHTBLACK_EX'])
+        print_cw(f"[+] Attaching {file}.{ext}{' '*len(data)}", cdict['LIGHTBLACK_EX'])
         sleep(0.8)
 
 def main(data, sdl, thrust):
 
-    thread2 = threading.Thread(target=weirdShit, args=(data, sdl, thrust))
-    thread1 = threading.Thread(target=data_generate, args=(data,))
+    thread2 = threading.Thread(target=graphic_gen, args=(data, sdl, thrust))
+    thread1 = threading.Thread(target=data_generator, args=(data,))
 
     threads =[thread1, thread2]
 
