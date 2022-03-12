8e9315c5b6f154949d9a6b235bc45625673822a1
SynAcktraa <harshmetaexploit90@gmail.com>
2022-03-13 01:43:11 +0530

    cleaned cdict

--- weirdshit.py
+++ weirdshit.py
@@ -1,41 +1,13 @@
 import time
 from sys import stdout
-import os
-
-"""
-🥴 Ansi codes weren't working in linux terminal, after using os.system("") it works like a charm 🥴
-"""
-os.system("")
+from colorext import fore, RESET
 
 
-cdict = {
-    
-    "BLACK":'\u001b[30m',
-    "RED":'\u001b[31m',	
-    "WHITE":'\u001b[37m',
-    "YELLOW":'\u001b[33m',
-    "BLUE":'\u001b[34m',
-    "GREEN":'\u001b[32m',	
-    "MAGENTA":'\u001b[35m',	
-    "CYAN":'\u001b[36m',	
-    "LIGHTGREEN_EX":'\u001b[32;1m',
-    "LIGHTCYAN_EX":'\u001b[36;1m',
-    "LIGHTBLUE_EX":'\u001b[34;1m',
-    "LIGHTRED_EX":'\u001b[31;1m',
-    "LIGHTMAGENTA_EX":'\u001b[35;1m',
-    "LIGHTYELLOW_EX":'\u001b[33;1m',
-    "LIGHTWHITE_EX":'\u001b[37;1m',
-    "LIGHTBLACK_EX":'\u001b[30;1m'
-}
-
-RESET = '\u001b[0m'
-	
-
-def stdout_cw(data, color=cdict['LIGHTWHITE_EX']):
+def stdout_fcw(data, color=fore.WHITE): #Just for foreground/text color
 
     stdout.write(f"{color}{data}{RESET}")
 
-def print_cw(data, color=cdict['LIGHTWHITE_EX']):
+def print_fcw(data, color=fore.WHITE): #Just for foreground/text color
 
         print(f"{color}{data}{RESET}")    
     
@@ -70,10 +42,10 @@ def graphic_gen(data_string="[+] loading script", schedule=3, thrust=4):
         for x in range(len(data)):
 
             s = '\r' + data[0:x] + upperstr[x] + data[x+1:] + '\r'
-            stdout_cw(s, cdict['LIGHTGREEN_EX'])
-            stdout_cw(f"{indent}...")
-            stdout_cw(next(spinner)) 
-            stdout_cw("\r")
+            stdout_fcw(s, fore.LIGHTGREEN_EX)
+            stdout_fcw(f"{indent}...")
+            stdout_fcw(next(spinner)) 
+            stdout_fcw("\r")
             stdout.flush()
             time.sleep(0.5/thrust)
 
